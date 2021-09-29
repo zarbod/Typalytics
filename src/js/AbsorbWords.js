170c274742c83c8de58ff1c92c1e20412380b824
GMG801 <89880031+GMG801@users.noreply.github.com>
2021-09-29 14:27:46 -0500

    saving

--- src/js/AbsorbWords.js
+++ src/js/AbsorbWords.js
@@ -1,5 +1,16 @@
+var prnt = "";
 chrome.tabs.executeScript({
-    file: "js/content.js"
+    file: "src/js/content.js"
+});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === 'done') {
+
+    }
+});
+chrome.storage.sync.get(["words"], function(result){
+    if(result.words.length) {
+        document.getElementById("count").innerHTML = result.words.length;
+    }
 });
 window.onclick = function (event) {
     let target = event.target;
@@ -7,10 +18,26 @@ window.onclick = function (event) {
         let clickedEle = document.activeElement.id;
         let ele = document.getElementById(clickedEle);
         if(ele === document.getElementById("practiceButton")){
-            alert("Starting Practice");
+            chrome.storage.sync.set({words: []}, function(){
+                alert("clearing");
+                document.getElementById("count").innerHTML = "0";
+            });
         }
+        if(ele === document.getElementById("refreshButton")){
+            chrome.tabs.executeScript({
+                file: "src/js/content.js"
+            });
+                    chrome.storage.sync.get(["words"], function(result){
+                        if(result.words.length) {
+                            document.getElementById("count").innerHTML = result.words.length;
+                            for(let i = 0; i < result.words.length; i++) {
+                                prnt = prnt + result.words[i] + " ";
+                            }
+                            //alert(prnt);
+                        }
+                    });
 
+        }
     }
 }
-
 //printing in the console of the extension
--- src/js/content.js
+++ src/js/content.js
@@ -1,5 +1,29 @@
-var words = [];
+var wrds = [];
+var addableWords = [];
+
 for(let i = 0; i < document.getElementsByClassName("replayWord").length; i++){
-    words.push(document.getElementsByClassName("replayWord")[i].innerHTML);
+    wrds.push(document.getElementsByClassName("replayWord")[i].innerHTML);
 }
-console.log(words);
+console.log(wrds);
+chrome.storage.sync.get(["words"], function(res){
+    if(res.words){
+        addableWords = res.words;
+        for(let word in wrds){
+            if(res.words.indexOf(wrds[word]) === -1) {
+                    addableWords.push(wrds[word]);
+                }
+            }
+        chrome.storage.sync.set({words: addableWords}, function(){
+           console.log("Adding total " + addableWords);
+        });
+    }else {
+        chrome.storage.sync.set({words: wrds}, function () {
+            //this runs when the set is complete
+            console.log("New Words :: " + wrds);
+
+            chrome.storage.sync.get(["words"], function (result) {
+                console.log(result.words);
+            });
+        });
+    }
+});
